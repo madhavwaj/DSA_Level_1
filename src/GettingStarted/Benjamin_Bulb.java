bdb5ecc03f8846a83ddede38eee1747d9ac0cb81
Madhav Bhardwaj <bhardwajmadhav003@gmail.com>
2024-09-25 23:26:12 +0530

    added Benjamin Bulb

---
+++ src/GettingStarted/Benjamin_Bulb.java
@@ -0,0 +1,14 @@
+package GettingStarted;
+
+import java.util.Scanner;
+
+public class Benjamin_Bulb {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i = 1; i * i <= n; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
