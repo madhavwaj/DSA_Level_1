285e027bd3418d1e490c23c03d75c6fe89ede4ba
Madhav Bhardwaj <bhardwajmadhav003@gmail.com>
2024-09-27 23:36:23 +0530

    Did these 2 questions

---
+++ src/GettingStarted/is_a_number_prime.java
@@ -0,0 +1,17 @@
+package GettingStarted;
+
+import java.util.Scanner;
+
+public class is_a_number_prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++){
+            int n = sc.nextInt();
+
+            for(int div = 2; div*div <= n; div++){
+            }
+        }
+    }
+}
