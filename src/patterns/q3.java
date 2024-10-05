5fe28b5de8ab413d776708a2915a42e6b74b5502
Madhav Bhardwaj <bhardwajmadhav003@gmail.com>
2024-10-05 22:20:06 +0530

    Pattern Problems solved 4 to 8

--- src/patterns/q3.java
+++ src/patterns/q3.java
@@ -1,4 +1,23 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int star = 1;
+        int space = n-1;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=space;j++){
+                System.out.print("\t");
+            }
+            for(int j =1; j<=star; j++){
+                System.out.print("*\t");
+            }
+            space--;
+            star++;
+            System.out.println();
+        }
+    }
 }
---
+++ src/patterns/q5.java
@@ -0,0 +1,29 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int st = 1;
+        int sp = n/2;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sp; j++){
+                System.out.print("\t");
+            }
+            for(int j=1; j<=st; j++){
+                System.out.print("*\t");
+            }
+            if(i<=n/2){
+                sp--;
+                st= st+2;
+            }else{
+                sp++;
+                st= st-2;
+            }
+            System.out.println();
+        }
+
+    }
+}
